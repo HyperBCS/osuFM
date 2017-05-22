0c827d1f5ea32646322880c00fb2ed1594763d54
BuildTools <atomicbcs@gmail.com>
2017-05-22 19:02:15 -0400

    Got good algorithm, fixed some null errors and made searching faster

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -332,7 +332,7 @@ for i in range(0,threads):
     else:
         arg.append({'start': current,'pages': size,'mode': mode})
         current += size 
-# results = pool.map(fetchMode, arg)
+results = pool.map(fetchMode, arg)
 print("Beginning score processing...")
 with db.transaction():
     for m in Beatmaps.select():
