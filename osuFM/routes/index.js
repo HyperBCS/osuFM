ed8a103ac27c58e30d7921bf2c2f0684bc34bccd
Brandon S <atomicbcs@gmail.com>
2020-09-14 22:13:23 -0400

    Update desc text

--- osuFM/routes/index.js
+++ osuFM/routes/index.js
@@ -34,7 +34,7 @@ var defaults = {
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('footer', {
-        title: 'osuFM - Find the best osu! farm maps',
+        title: 'osu!FM - Find the best osu! farm maps',
         date: req.app.locals.dbUpdateDate
     });
 });
