9be65067c71e0e4b97f8b49ac7f39e3869f72407
Brandon S <atomicbcs@gmail.com>
2020-09-16 01:27:58 -0400

    fix mod selector

--- osuFM/osufm.config.js
+++ osuFM/osufm.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'osuFM',
     script: './bin/www',
     autorestart: true,
+    max_memory_restart: '1G',
     watch: ["osuFM.db"],
     env: {
       NODE_ENV: 'production'
